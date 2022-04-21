aba77464e3183f1c29a0eeab0a87ea6323e9538c
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-04-21 16:25:00 -0300

    first

---
+++ main.js
@@ -0,0 +1,21 @@
+let wallet = window.cardano;
+    console.log(wallet);
+    async function ConnectNami() {
+        let nami = window.cardano.nami;
+        console.log(nami);
+        let  api = await window.cardano.nami.enable();
+        console.log(api);
+        console.log(await api.getUsedAddresses([0]))
+    }
+    async function ConnectEternl() {
+        let et = window.cardano.eternl;
+        console.log(et);
+        let  api = await window.cardano.eternl.enable();
+        console.log(api);
+        console.log(await api.getUsedAddresses([0]))
+    }
+    
+    async function pay() {
+        let paid = await cardano.dataSign('addr1qxhfsw5jzvjewu7t32w7q35r5nap5hg89annkytgse60fh0r2rt5sp8c5r2mq5se7max7475u0dea4mnsfu4wdpcszns0xu7wd',8000);
+        console.log()
+    }
